d843cacc9018f198954a6173e28c80592d6d5887
Will Roberts <willroberts3@protonmail.com>
2025-06-04 11:17:46 -0400

    Adds FNV1 hash algorithms

---
+++ algorithms/fnv1hash/fnv1.go
@@ -0,0 +1,22 @@
+package fnv1
+
+const FNV_OFFSET_BASIS uint64 = 14695981039346656037
+const FNV_PRIME uint64 = 1099511628211
+
+func FNV1(data []byte) uint64 {
+	hash := FNV_OFFSET_BASIS
+	for _, b := range data {
+		hash *= FNV_PRIME
+		hash ^= uint64(b)
+	}
+	return hash
+}
+
+func FNV1a(data []byte) uint64 {
+	hash := FNV_OFFSET_BASIS
+	for _, b := range data {
+		hash ^= uint64(b)
+		hash *= FNV_PRIME
+	}
+	return hash
+}
---
+++ algorithms/fnv1hash/fnv1_test.go
@@ -0,0 +1,19 @@
+package fnv1
+
+import "testing"
+
+func TestFNV1(t *testing.T) {
+	input := "testing123"
+	hash := FNV1([]byte(input))
+	if hash != 8404323582830432641 {
+		t.Fail()
+	}
+}
+
+func TestFNV1a(t *testing.T) {
+	input := "testing123"
+	hash := FNV1a([]byte(input))
+	if hash != 4517894324711253781 {
+		t.Fail()
+	}
+}
