4a28f7c5f5168a42f588b131905c5e9a35608310
Will Roberts <willroberts3@protonmail.com>
2025-06-06 10:29:43 -0400

    Adds pre- and post-order traversal

--- datastructures/graphs/tree.go
+++ datastructures/graphs/tree.go
@@ -1,5 +1,14 @@
 package graphs
 
+// Binary Search Tree: Smaller values always to the left, greater always to the right.
+// - Exception: Duplicate values: duplicates can be either left or right aligned, must be specified.
+// Unbalanced Tree: No enforcement of branch lengths.
+// Balanced Tree: Balanced enough to ensure O(log n) runtime for insert and find operations.
+// - Examples: Red-Black Tree, AVL Tree.
+// Complete Binary Tree: All values are filled. Last level may be unfilled as long as values are filled left-to-right.
+// Full Binary Tree: All nodes have 0 or 2 children, not 1.
+// Perfect Tree: Both Full and Complete. Exact same number of nodes in each branch.
+
 type Tree struct {
 	root *Node
 }
@@ -27,6 +36,7 @@ func (n *BinaryNode) IsLeaf() bool {
 	return n.left == nil && n.right == nil
 }
 
+// TraverseInOrder yields values from left nodes before current and right nodes.
 func TraverseInOrder(n *BinaryNode, valuesCh chan any) {
 	if n != nil {
 		TraverseInOrder(n.left, valuesCh)
@@ -35,11 +45,20 @@ func TraverseInOrder(n *BinaryNode, valuesCh chan any) {
 	}
 }
 
-// Binary Search Tree: Smaller values always to the left, greater always to the right.
-// - Exception: Duplicate values: duplicates can be either left or right aligned, must be specified.
-// Unbalanced Tree: No enforcement of branch lengths.
-// Balanced Tree: Balanced enough to ensure O(log n) runtime for insert and find operations.
-// - Examples: Red-Black Tree, AVL Tree.
-// Complete Binary Tree: All values are filled. Last level may be unfilled as long as values are filled left-to-right.
-// Full Binary Tree: All nodes have 0 or 2 children, not 1.
-// Perfect Tree: Both Full and Complete. Exact same number of nodes in each branch.
+// TraversePreOrder yields values from current node before left and right nodes.
+func TraversePreOrder(n *BinaryNode, valuesCh chan any) {
+	if n != nil {
+		valuesCh <- n.value
+		TraverseInOrder(n.left, valuesCh)
+		TraverseInOrder(n.right, valuesCh)
+	}
+}
+
+// TraversePostOrder yields values from left and right nodes before current node.
+func TraversePostOrder(n *BinaryNode, valuesCh chan any) {
+	if n != nil {
+		TraverseInOrder(n.left, valuesCh)
+		TraverseInOrder(n.right, valuesCh)
+		valuesCh <- n.value
+	}
+}
--- datastructures/graphs/tree_test.go
+++ datastructures/graphs/tree_test.go
@@ -14,30 +14,12 @@ func TestTree(t *testing.T) {
 	}
 }
 
-func TestBinaryTree(t *testing.T) {
-	_ = BinaryTree{
-		root: &BinaryNode{
-			value: 0,
-			left:  nil,
-			right: nil,
-		},
-	}
-}
-
 func TestTraverseInOrder(t *testing.T) {
 	tree := BinaryTree{
 		root: &BinaryNode{
 			value: 8,
-			left: &BinaryNode{
-				value: 4,
-				left:  &BinaryNode{value: 2},
-				right: &BinaryNode{value: 6},
-			},
-			right: &BinaryNode{
-				value: 12,
-				left:  &BinaryNode{value: 10},
-				right: &BinaryNode{value: 14},
-			},
+			left:  &BinaryNode{value: 4, left: &BinaryNode{value: 2}, right: &BinaryNode{value: 6}},
+			right: &BinaryNode{value: 12, left: &BinaryNode{value: 10}, right: &BinaryNode{value: 14}},
 		},
 	}
 	valuesCh := make(chan any, 7)
@@ -45,6 +27,45 @@ func TestTraverseInOrder(t *testing.T) {
 	for i := 1; i <= 7; i++ {
 		v := <-valuesCh
 		if v != i*2 {
+			t.Log("expected", i*2, "got", v)
+			t.Fail()
+		}
+	}
+}
+
+func TestTraversePreOrder(t *testing.T) {
+	tree := BinaryTree{
+		root: &BinaryNode{
+			value: 2,
+			left:  &BinaryNode{value: 6, left: &BinaryNode{value: 4}, right: &BinaryNode{value: 8}},
+			right: &BinaryNode{value: 12, left: &BinaryNode{value: 10}, right: &BinaryNode{value: 14}},
+		},
+	}
+	valuesCh := make(chan any, 7)
+	TraversePreOrder(tree.root, valuesCh)
+	for i := 1; i <= 7; i++ {
+		v := <-valuesCh
+		if v != i*2 {
+			t.Log("expected", i*2, "got", v)
+			t.Fail()
+		}
+	}
+}
+
+func TestTraversePostOrder(t *testing.T) {
+	tree := BinaryTree{
+		root: &BinaryNode{
+			value: 14,
+			left:  &BinaryNode{value: 4, left: &BinaryNode{value: 2}, right: &BinaryNode{value: 6}},
+			right: &BinaryNode{value: 10, left: &BinaryNode{value: 8}, right: &BinaryNode{value: 12}},
+		},
+	}
+	valuesCh := make(chan any, 7)
+	TraversePostOrder(tree.root, valuesCh)
+	for i := 1; i <= 7; i++ {
+		v := <-valuesCh
+		if v != i*2 {
+			t.Log("expected", i*2, "got", v)
 			t.Fail()
 		}
 	}
