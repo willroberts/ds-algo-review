7065d1d31a654c957d77f623a86d6410a442dced
Will Roberts <willroberts3@protonmail.com>
2025-06-09 08:41:17 -0400

    Reworks minheap implementation

--- datastructures/graphs/heap.go
+++ datastructures/graphs/heap.go
@@ -1,44 +1,94 @@
 package graphs
 
+import "errors"
+
 // MinHeap is a complete binary tree where each node is smaller than its children.
 // Can be used to implement a priority queue.
+// For this implementation, our internal data structure is a slice, instead of using
+// custom data structures with pointers to nodes. This is possible since the tree is
+// complete, and it simplifies append and swap operations.
 // For the standard library implementation, import 'container/heap'.
 type MinHeap struct {
-	root *MinHeapNode
+	items []int
 }
 
-type MinHeapNode struct {
-	value int
-	left  *MinHeapNode
-	right *MinHeapNode
+func (h *MinHeap) GetParent(i int) (int, error) {
+	if i == 0 {
+		return 0, errors.New("cannot get parent of root node")
+	}
+	// Go rounds down on integer division. Add 1 for odd inputs.
+	if i%2 == 1 {
+		i += 1
+	}
+	return (i - 2) / 2, nil
 }
 
-func (n *MinHeapNode) Value() any {
-	return n.value
+func (h *MinHeap) GetLeftChild(i int) int {
+	return i*2 + 1
 }
 
-func (n *MinHeapNode) Left() *MinHeapNode {
-	return n.left
+func (h *MinHeap) GetRightChild(i int) int {
+	return i*2 + 2
 }
 
-func (n *MinHeapNode) Right() *MinHeapNode {
-	return n.right
+// Peek returns the first element in the heap.
+func (h *MinHeap) Peek() (int, error) {
+	if len(h.items) == 0 {
+		return 0, errors.New("heap is empty")
+	}
+	return h.items[0], nil
+}
+
+func (h *MinHeap) Value(i int) int {
+	return h.items[i]
+}
+
+func (h *MinHeap) SwapValues(i, j int) {
+	v := h.items[i]
+	h.items[i] = h.items[j]
+	h.items[j] = v
 }
 
 // Insert places the given element at the bottom of the tree before calling Fix().
-func (h *MinHeap) Insert(v int) {
-	h.Fix()
+func (h *MinHeap) Insert(i int) {
+	h.items = append(h.items, i)
+	h.heapifyUp()
 }
 
-// Fix ensures correct ordering by bubbling up the element to its correct position.
-func (h *MinHeap) Fix() {
-
+// ExtractMin removes the minimum element from the heap and rebalances the tree.
+func (h *MinHeap) ExtractMin() (int, error) {
+	if len(h.items) == 0 {
+		return 0, errors.New("heap is empty")
+	}
+	v := h.items[0]
+	// Move the last element to the root and shrink the heap.
+	h.items[0] = h.items[len(h.items)-1]
+	h.items = h.items[:len(h.items)-1]
+	// Re-heapify.
+	h.heapifyDown()
+	return v, nil
 }
 
-func (h *MinHeap) ExtractMin() {
-
+func (h *MinHeap) heapifyUp() error {
+	if len(h.items) == 0 {
+		return errors.New("heap is empty")
+	}
+	i := len(h.items) - 1
+	for {
+		if i == 0 {
+			break
+		}
+		parent, _ := h.GetParent(i)
+		if i > 0 && h.items[parent] > h.items[i] {
+			h.SwapValues(parent, i)
+			i = parent
+		} else {
+			break
+		}
+	}
+	return nil
 }
 
-// MaxHeap is a complete binary tree where each node is larger than its children.
-type MaxHeap struct {
+func (h *MinHeap) heapifyDown() {
+
 }
--- datastructures/graphs/heap_test.go
+++ datastructures/graphs/heap_test.go
@@ -1,12 +1,50 @@
 package graphs
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestHeapInsert(t *testing.T) {
-	h := MinHeap{
-		root: &MinHeapNode{value: 0, left: &MinHeapNode{value: 1}, right: &MinHeapNode{value: 2}},
+func TestMinHeap_GetParent(t *testing.T) {
+	h := &MinHeap{items: make([]int, 0)}
+	if _, err := h.GetParent(0); err == nil {
+		t.Log("got nil, expected err")
+		t.Fail()
 	}
-	h.Insert(2)
+	if v, err := h.GetParent(1); err != nil || v != 0 {
+		t.Log("got", v, "expected", 0)
+		t.Fail()
+	}
+	if v, err := h.GetParent(2); err != nil || v != 0 {
+		t.Log("got", v, "expected", 0)
+		t.Fail()
+	}
+	if v, err := h.GetParent(3); err != nil || v != 1 {
+		t.Log("got", v, "expected", 1)
+		t.Fail()
+	}
+	if v, err := h.GetParent(4); err != nil || v != 1 {
+		t.Log("got", v, "expected", 1)
+		t.Fail()
+	}
+	if v, err := h.GetParent(5); err != nil || v != 2 {
+		t.Log("got", v, "expected", 2)
+		t.Fail()
+	}
+	if v, err := h.GetParent(6); err != nil || v != 2 {
+		t.Log("got", v, "expected", 2)
+		t.Fail()
+	}
+}
+
+func TestMinHeap_Insert(t *testing.T) {
+	h := &MinHeap{items: make([]int, 0)}
+	h.Insert(30)
+	h.Insert(20)
+	h.Insert(10)
+	expected := []int{10, 30, 20}
+	if !reflect.DeepEqual(h.items, expected) {
+		t.Log("expected", expected, "got", h.items)
+		t.Fail()
+	}
 }
