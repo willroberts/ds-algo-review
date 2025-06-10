512abb80dc5b297ceebe879a6ac0a14719e41ccf
Will Roberts <willroberts3@protonmail.com>
2025-06-09 20:44:42 -0400

    Adds basic graph implementation

---
+++ datastructures/graph/graph.go
@@ -0,0 +1,47 @@
+package graph
+
+import "container/list"
+
+type Graph struct {
+	nodeLookup map[int]*GraphNode
+}
+
+func (g *Graph) AddNode(node *GraphNode) {
+	g.nodeLookup[node.id] = node
+}
+
+func (g *Graph) GetNode(id int) *GraphNode {
+	return g.nodeLookup[id]
+}
+
+func (g *Graph) AddEdge(src int, dst int) {
+	s := g.GetNode(src)
+	d := g.GetNode(dst)
+	s.MarkAdjacent(d)
+}
+
+func NewGraph() *Graph {
+	return &Graph{
+		nodeLookup: make(map[int]*GraphNode, 0),
+	}
+}
+
+type GraphNode struct {
+	id       int
+	adjacent *list.List
+}
+
+func (n *GraphNode) MarkAdjacent(node *GraphNode) {
+	n.adjacent.PushBack(node)
+}
+
+func (n *GraphNode) GetAdjacent() *list.List {
+	return n.adjacent
+}
+
+func NewGraphNode(id int) *GraphNode {
+	return &GraphNode{
+		id:       id,
+		adjacent: &list.List{},
+	}
+}
---
+++ datastructures/graph/graph_test.go
@@ -0,0 +1,17 @@
+package graph
+
+import "testing"
+
+func TestGraph(t *testing.T) {
+	g := NewGraph()
+	n1 := NewGraphNode(1)
+	n2 := NewGraphNode(2)
+	g.AddNode(n1)
+	g.AddNode(n2)
+	g.AddEdge(1, 2)
+
+	// Assert edge exists between n1 and n2.
+	if n1.GetAdjacent().Front().Value != n2 {
+		t.Fail()
+	}
+}
