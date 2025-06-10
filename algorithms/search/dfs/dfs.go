653479b7507106809ed68022255a9efc4965d56d
Will Roberts <willroberts3@protonmail.com>
2025-06-09 21:43:02 -0400

    Adds recursive depth-first search implementation

---
+++ algorithms/search/dfs/dfs.go
@@ -0,0 +1,27 @@
+package bfs
+
+import (
+	"github.com/willroberts/ds-algo-review/datastructures/graph"
+)
+
+// DepthFirstSearch searches a Graph structure's edges recursively.
+// Returns 'true' when a path exists between the given nodes.
+func DepthFirstSearch(g *graph.Graph, src int, dst int) bool {
+	return depthFirstSearch(g.GetNode(src), g.GetNode(dst), make(map[int]struct{}, 0))
+}
+
+func depthFirstSearch(src *graph.GraphNode, dst *graph.GraphNode, visited map[int]struct{}) bool {
+	if _, ok := visited[src.ID()]; ok {
+		return false
+	}
+	visited[src.ID()] = struct{}{}
+	if src == dst {
+		return true
+	}
+	for e := src.GetAdjacent().Front(); e != nil; e = e.Next() {
+		if depthFirstSearch(e.Value.(*graph.GraphNode), dst, visited) {
+			return true
+		}
+	}
+	return false
+}
---
+++ algorithms/search/dfs/dfs_test.go
@@ -0,0 +1,43 @@
+package bfs
+
+import (
+	"testing"
+
+	"github.com/willroberts/ds-algo-review/datastructures/graph"
+)
+
+func TestDepthFirstSearch(t *testing.T) {
+	// Graph:
+	// n1 -> n2 -> n4 -> n6
+	//    -> n3 -> n5
+	g := graph.NewGraph()
+	n1 := graph.NewGraphNode(1)
+	g.AddNode(n1)
+	n2 := graph.NewGraphNode(2)
+	g.AddNode(n2)
+	n3 := graph.NewGraphNode(3)
+	g.AddNode(n3)
+	n4 := graph.NewGraphNode(4)
+	g.AddNode(n4)
+	n5 := graph.NewGraphNode(5)
+	g.AddNode(n5)
+	n6 := graph.NewGraphNode(6)
+	g.AddNode(n6)
+	g.AddEdge(1, 2)
+	g.AddEdge(2, 4)
+	g.AddEdge(4, 6)
+	g.AddEdge(1, 3)
+	g.AddEdge(3, 5)
+
+	// Assert a path exists from n1 to n6.
+	if !DepthFirstSearch(g, 1, 6) {
+		t.Log("a path does not exist from n1 to n6")
+		t.Fail()
+	}
+
+	// Assert no path exists from n3 to n6.
+	if DepthFirstSearch(g, 3, 6) {
+		t.Log("a path exists from n3 to n6")
+		t.Fail()
+	}
+}
