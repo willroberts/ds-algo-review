18166ae36b556724e2ef42b7b73aa520723952da
Will Roberts <willroberts3@protonmail.com>
2025-06-10 10:26:03 -0400

    Adds comments

--- algorithms/sort/pdqsort/pdqsort.go
+++ algorithms/sort/pdqsort/pdqsort.go
@@ -1,1 +1,7 @@
 package pdqsort
+
+// Golang's slices.Sort method uses a pattern-defeating QuickSort (pdqsort) algorithm internally.
+// pdqsort combines the fast average case of QuickSort with the fast worst case of heapsort.
+func PDQSort(array []int) {
+
+}
